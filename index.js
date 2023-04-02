279a0382793bfdeefe5593194c2905c1bdc58e5b
Agostina Armella <agos_armella@hotmail.es>
2023-04-02 11:22:27 -0300

    Agrego formulario y uso del JSON

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+const formulario = document.getElementById('formulario')
+const inputNombre = document.getElementById('nombre')
+const inputApellido = document.getElementById('apellido')
+const titulo = document.getElementById ('titulo')
+
+formulario.onsubmit = (e) => {
+    e.preventDefault()
+    const infoUsuario = {
+        nombre: inputNombre.value,
+        apellido: inputApellido.value
+    }
+    localStorage.setItem('infoUsuario', JSON.stringify(infoUsuario))
+    formulario.remove()
+    titulo.innerText = `Bienvenido ${infoUsuario.nombre} ${infoUsuario.apellido}`
+}
+
+const infoUsuario = localStorage.getItem('infoUsuario')
+const infoUsuarioJS = JSON.parse(infoUsuario)
+if (infoUsuario) {
+    formulario.remove()
+    titulo.innerText = `Bienvenido ${infoUsuarioJS.nombre} ${infoUsuarioJS.apellido}`
+}
 
 class Producto {
     constructor(id, nombre, precio) {
@@ -8,8 +30,8 @@ class Producto {
 }
 
 const Productos = [
-    new Producto(1,'Iphone', 100000 ),
-    new Producto(2, 'Tablet',80000),
+    new Producto(1, 'Iphone', 100000),
+    new Producto(2, 'Tablet', 80000),
     new Producto(3, 'Tv', 150000),
     new Producto(4, 'Airpods', 20000)
 ]
@@ -24,62 +46,23 @@ Productos.forEach((prod) => {
 
 const carrito = []
 
-const anadirBtnNode = document.querySelector ('#anadirprods')
-anadirBtnNode.onclick = ()=>{
+const anadirBtnNode = document.querySelector('#anadirprods')
+anadirBtnNode.onclick = () => {
     const index = selectNode.selectedIndex
-    const Prodseleccionado = Productos [index]
+    const Prodseleccionado = Productos[index]
     carrito.push(Prodseleccionado)
     console.log(carrito)
 }
 
-const finalizarBtnNode = document.querySelector ('#finalizarcompra')
-finalizarBtnNode.onclick = ()=>{
+const finalizarBtnNode = document.querySelector('#finalizarcompra')
+finalizarBtnNode.onclick = () => {
     let totalcompra = 0
-    carrito.forEach ((Productos) => {
-        totalcompra = totalcompra +Productos.precio
+    carrito.forEach((Productos) => {
+        totalcompra = totalcompra + Productos.precio
     })
     alert(
-            `El total de tu compra es ${totalcompra}`
-         )
+        `El total de tu compra es ${totalcompra}`
+    )
 }
 
 
-
-
-
-// let BancoDeseado = parseInt(
-//     prompt('Escoge el numero del Banco que desea cotizar 1.Banco Patagonia - 2.Banco Credicoop - 3.Banco Ciudad - 4.Banco Nacion')
-// )
-
-// let Bancoescogido = false
-// let banco
-
-// while (Bancoescogido === false) {
-//     banco = bancos.find((banco => banco.id === BancoDeseado))
-//     if (!banco) {
-//         BancoDeseado = parseInt(
-//             prompt('Escoge el numero correcto del Banco que desea cotizar 1.Banco Patagonia - 2.Banco Credicoop - 3.Banco Ciudad - 4.Banco Nacion')
-//         )
-//     } else {
-//         Bancoescogido = true
-//     }
-// }
-// console.log(banco);
-
-// const montosolicitado = parseInt(
-//    prompt('Ingresa el monto que deseas solicitar')
-// )
-
-// const cuota12 = calcularCuota(montosolicitado, banco.tasa12, 12)
-// const cuota24 = calcularCuota(montosolicitado, banco.tasa12, 24)
-// const cuota36 = calcularCuota(montosolicitado, banco.tasa12, 36)
-
-// function calcularCuota(monto, interes, meses) {
-//     const cuota = ((monto + (monto * interes)) / 100) / meses
-//     return cuota
-// }
-
-// alert(
-//     `Las opciones que te ofrece ${banco.nombre} son: 1. ${cuota12} mensuales a 12 meses -2. ${cuota24} mensuales a 24 meses -3. ${cuota36} mensuales a 36 meses`
-// )
-// }
