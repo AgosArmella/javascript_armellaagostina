72c619d2e3c6633169686b61b5e8d5e0d27c7c32
Agostina Armella <agos_armella@hotmail.es>
2023-04-01 21:09:19 -0300

    Modificaciones en el proyecto, html y js y aplico DOM

--- index.js
+++ index.js
@@ -1,56 +1,85 @@
-// un usuario elija un banco y acceda a las distintas cuotas segun los meses y el monto
 
-class Banco {
-    constructor(nombre, id, tasa12, tasa24, tasa36) {
-        this.nombre = nombre
+class Producto {
+    constructor(id, nombre, precio) {
         this.id = id
-        this.tasa12 = tasa12
-        this.tasa24 = tasa24
-        this.tasa36 = tasa36
+        this.nombre = nombre
+        this.precio = precio
     }
 }
 
-const bancos = [
-    new Banco('Banco Patagonia', 1, 3, 10, 15),
-    new Banco('Banco Credicoop', 2, 10, 12, 15),
-    new Banco('Banco Ciudad', 3, 5, 9, 11),
-    new Banco('Banco Nacion', 4, 5, 10, 20)
+const Productos = [
+    new Producto(1,'Iphone', 100000 ),
+    new Producto(2, 'Tablet',80000),
+    new Producto(3, 'Tv', 150000),
+    new Producto(4, 'Airpods', 20000)
 ]
 
-let BancoDeseado = parseInt(
-    prompt('Escoge el numero del Banco que desea cotizar 1.Banco Patagonia - 2.Banco Credicoop - 3.Banco Ciudad - 4.Banco Nacion')
-)
+const selectNode = document.querySelector('#listaprods')
+Productos.forEach((prod) => {
+    const optionprod = document.createElement('option')
+    optionprod.innerText = ` ${prod.nombre}: $${prod.precio} `
+    optionprod.setAttribute('id', ` ${prod.id} `)
+    selectNode.append(optionprod)
+})
 
-let Bancoescogido = false
-let banco
+const carrito = []
 
-while (Bancoescogido === false) {
-    banco = bancos.find((banco => banco.id === BancoDeseado))
-    if (!banco) {
-        BancoDeseado = parseInt(
-            prompt('Escoge el numero correcto del Banco que desea cotizar 1.Banco Patagonia - 2.Banco Credicoop - 3.Banco Ciudad - 4.Banco Nacion')
-        )
-    } else {
-        Bancoescogido = true
-    }
-}
-console.log(banco);
-
-const montosolicitado = parseInt(
-   prompt('Ingresa el monto que deseas solicitar')
-)
-
-const cuota12 = calcularCuota(montosolicitado, banco.tasa12, 12)
-const cuota24 = calcularCuota(montosolicitado, banco.tasa12, 24)
-const cuota36 = calcularCuota(montosolicitado, banco.tasa12, 36)
-
-function calcularCuota(monto, interes, meses) {
-    const cuota = ((monto + (monto * interes)) / 100) / meses
-    return cuota
+const anadirBtnNode = document.querySelector ('#anadirprods')
+anadirBtnNode.onclick = ()=>{
+    const index = selectNode.selectedIndex
+    const Prodseleccionado = Productos [index]
+    carrito.push(Prodseleccionado)
+    console.log(carrito)
 }
 
-alert(
-    `Las opciones que te ofrece ${banco.nombre} son: 1. ${cuota12} mensuales a 12 meses -2. ${cuota24} mensuales a 24 meses -3. ${cuota36} mensuales a 36 meses`
-)
+const finalizarBtnNode = document.querySelector ('#finalizarcompra')
+finalizarBtnNode.onclick = ()=>{
+    let totalcompra = 0
+    carrito.forEach ((Productos) => {
+        totalcompra = totalcompra +Productos.precio
+    })
+    alert(
+            `El total de tu compra es ${totalcompra}`
+         )
+}
 
 
+
+
+
+// let BancoDeseado = parseInt(
+//     prompt('Escoge el numero del Banco que desea cotizar 1.Banco Patagonia - 2.Banco Credicoop - 3.Banco Ciudad - 4.Banco Nacion')
+// )
+
+// let Bancoescogido = false
+// let banco
+
+// while (Bancoescogido === false) {
+//     banco = bancos.find((banco => banco.id === BancoDeseado))
+//     if (!banco) {
+//         BancoDeseado = parseInt(
+//             prompt('Escoge el numero correcto del Banco que desea cotizar 1.Banco Patagonia - 2.Banco Credicoop - 3.Banco Ciudad - 4.Banco Nacion')
+//         )
+//     } else {
+//         Bancoescogido = true
+//     }
+// }
+// console.log(banco);
+
+// const montosolicitado = parseInt(
+//    prompt('Ingresa el monto que deseas solicitar')
+// )
+
+// const cuota12 = calcularCuota(montosolicitado, banco.tasa12, 12)
+// const cuota24 = calcularCuota(montosolicitado, banco.tasa12, 24)
+// const cuota36 = calcularCuota(montosolicitado, banco.tasa12, 36)
+
+// function calcularCuota(monto, interes, meses) {
+//     const cuota = ((monto + (monto * interes)) / 100) / meses
+//     return cuota
+// }
+
+// alert(
+//     `Las opciones que te ofrece ${banco.nombre} son: 1. ${cuota12} mensuales a 12 meses -2. ${cuota24} mensuales a 24 meses -3. ${cuota36} mensuales a 36 meses`
+// )
+// }
