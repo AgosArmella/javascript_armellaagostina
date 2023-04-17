61c9705cf049ccc5279c2c1e18560de29e9e722b
Agostina Armella <agos_armella@hotmail.es>
2023-04-17 13:30:14 -0300

    agrego archivo json y librerias

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const formulario = document.getElementById('formulario')
 const inputNombre = document.getElementById('nombre')
 const inputApellido = document.getElementById('apellido')
-const titulo = document.getElementById ('titulo')
+const titulo = document.getElementById('titulo')
 
 formulario.onsubmit = (e) => {
     e.preventDefault()
@@ -29,35 +29,41 @@ class Producto {
     }
 }
 
-const Productos = [
-    new Producto(1, 'Iphone', 100000),
-    new Producto(2, 'Tablet', 80000),
-    new Producto(3, 'Tv', 150000),
-    new Producto(4, 'Airpods', 20000)
-]
+async function getProductos() {
+    const response = await fetch('./productos.json');
+    const resultado = await response.json();
+    console.log(resultado.productos); 
+    return resultado.productos;
+}
 
-const selectNode = document.querySelector('#listaprods')
-Productos.forEach((prod) => {
-    const optionprod = document.createElement('option')
-    optionprod.innerText = ` ${prod.nombre}: $${prod.precio} `
-    optionprod.setAttribute('id', ` ${prod.id} `)
-    selectNode.append(optionprod)
-})
+async function mostrarProductos() {
+    const productos = await getProductos();
+    productos.forEach(prod => {
+        console.log(prod);
+        const optionprod = document.createElement('option')
+        optionprod.innerText = ` ${prod.nombre}: $${prod.precio} `
+        optionprod.setAttribute('id', ` ${prod.id} `)
+        selectNode.append(optionprod)
+    });
+}
 
 const carrito = []
+mostrarProductos()
+const selectNode = document.querySelector('#listaprods')
 
 const anadirBtnNode = document.querySelector('#anadirprods')
 anadirBtnNode.onclick = () => {
     const index = selectNode.selectedIndex
-    const Prodseleccionado = Productos[index]
+    const Prodseleccionado = Producto[index]
     carrito.push(Prodseleccionado)
+    toastr.success(`${producto.nombre} agregado al carrito`,'Mensaje');
     console.log(carrito)
 }
 
 const finalizarBtnNode = document.querySelector('#finalizarcompra')
 finalizarBtnNode.onclick = () => {
     let totalcompra = 0
-    carrito.forEach((Productos) => {
+    carrito.forEach((Producto) => {
         totalcompra = totalcompra + Productos.precio
     })
     alert(
